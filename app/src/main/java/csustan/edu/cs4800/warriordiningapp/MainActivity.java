7d472049e0b939fd477a96f4691d97356fe237d7
Dan C <143655990+Dan-Land@users.noreply.github.com>
2023-10-23 15:50:37 -0700

    test commit
    will it work?

---
+++ app/src/main/java/csustan/edu/cs4800/warriordiningapp/MainActivity.java
@@ -0,0 +1,14 @@
+package csustan.edu.cs4800.warriordiningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
